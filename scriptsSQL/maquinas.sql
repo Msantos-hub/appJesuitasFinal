5b87944d8a421a2692e91f486151000bdc2c10e9
Msantos-hub <msantosgarcia.guadalupe@alumnado.fundacionloyola.net>
2021-06-11 20:13:52 +0200

    New version

--- scriptsSQL/maquinas.sql
+++ scriptsSQL/maquinas.sql
@@ -1,10 +1,10 @@
 USE appJesuita;
 
-INSERT INTO Maquina (ip,nombreAlumno,usuario,idJesuita,idLugar,tipo,idUsuario,password) VALUES
+INSERT INTO Maquina (ip,UsuarioAlumno,usuario,idJesuita,idLugar,tipo,idUsuario,password) VALUES
 ('10.10.10.99','admin','admin',NULL,NULL,'a',0,'123456'),
-('10.10.10.0','Alumno1','Nombre1','10.10.10.0',1,'u',1,'1324'),
-('10.10.10.1','Alumno2','Nombre2','10.10.10.1',2,'u',2,'1324'),
-('10.10.10.2','Alumno3','Nombre3','10.10.10.2',3,'u',3,'1324'),
-('10.10.10.3','Alumno4','Nombre4','10.10.10.3',4,'u',4,'1324'),
-('10.10.10.4','Alumno5','Nombre5','10.10.10.4',5,'u',5,'1324'),
-('10.10.10.5','Alumno6','Nombre6','10.10.10.5',6,'u',6,'1324');
+('10.10.10.1','Alumno1','Usuario1','10.10.10.0',1,'u',1,'1324'),
+('10.10.10.2','Alumno2','Usuario2','10.10.10.1',2,'u',2,'1324'),
+('10.10.10.3','Alumno3','Usuario3','10.10.10.2',3,'u',3,'1324'),
+('10.10.10.4','Alumno4','Usuario4','10.10.10.3',4,'u',4,'1324'),
+('10.10.10.5','Alumno5','Usuario5','10.10.10.4',5,'u',5,'1324'),
+('10.10.10.6','Alumno6','Usuario6','10.10.10.5',6,'u',6,'1324');
