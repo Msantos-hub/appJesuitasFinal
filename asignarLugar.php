6ce9c7927dacb353723eeccb8f99b6bd06efaed7
Msantos-hub <msantosgarcia.guadalupe@alumnado.fundacionloyola.net>
2021-06-12 16:18:35 +0200

    New version

--- asignarLugar.php
+++ asignarLugar.php
@@ -12,9 +12,54 @@
     </div>
 </nav>
 <div id="general">
-    <?php
+    <h3>Asignacion de Lugares a Jesuitas</h3>
+    <form method="post" id="formvisita">
+        <?php
+        include 'Operaciones.php';
+        $objeto = new Operaciones();
 
+        $sql="SELECT * FROM jesuita";
+        echo '<label>Jesuita</label>';
+        echo '<select name="nJesuita">';
+        $objeto->realizarConsultas($sql);
+        if($objeto->comprobarSelect()>0){
+            while($fila=$objeto->extraerFilas()){
+                echo '<option value="'.$fila['idJesuita'].'">'.$fila['nombre'].'</option>';
+            }
+        }else{
+            echo 'Consulta no se a realizado correctamente1';
+        }
+        echo '</select><br>';
+        echo '<label>Lugares</label>';
+        echo '<select name="nLugares">';
+        $sql2="SELECT * FROM Lugar";
+        $objeto->realizarConsultas($sql2);
+        if($objeto->comprobarSelect()>0){
+            while($fila=$objeto->extraerFilas()) {
+                echo '<option value="'.$fila['idLugar'].'">'.$fila['nombre'].'</option>';
+            }
+        }else{
+            echo 'Consulta no se a realizado correctamente2';
+        }
+        echo '</select><br><hr>';
+        echo '<input type="submit" value="Realizar Visita" name="enviar">  <a href="paginaInicio.php">Volver</a>';
+
+        if (isset($_POST['enviar'])){
+            $idjesuita=$_POST['nJesuita'];
+            $idlugar=$_POST['nLugar'];
+            $sql3="INSERT INTO maquina(idJesuita,idLugar) VALUES('".$idjesuita."',.$idlugar.)";
+            $objeto->realizarConsultas($sql3); //consulta de insercion de datos
+            if ($objeto->comprobar()>0) {
+                echo '<br>';
+                echo 'Asignacion relizada correctamente.';
+                echo '<br>';
+                echo 'Realice otra asignacion.';
+            } else {
+                echo 'La visita no se a realizado correctamente3';
+            }
+        }
     ?>
+    </form>
 </div>
 </body>
 </html>
--- paginaInicio.php
+++ paginaInicio.php
@@ -11,7 +11,6 @@
             <div id="link-1"><a href="paginaInicio.php">Inicio</a></div>
             <div id="link-2"><a href="visitas.php">Visitas</a></div>
             <div id="link-3"><a href="rankingvisitas.php">Rankings</a></div>
-            <div id="link-4"><a href="jesuitas.php">Jesuitas</a></div>
             <div id="link-5"><a href="logout.php">Cerrar Sesion</a></div>
         </div>
     </nav>
