6dff4119ff34e5e53c8dcb9f593869e0f41b1ba3
Msantos-hub <msantosgarcia.guadalupe@alumnado.fundacionloyola.net>
2021-06-12 14:05:40 +0200

    New version

---
+++ asignarLugar.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>inicio</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<nav>
+    <div id="menu">
+        <div id="link-1"><a href="../crud.html">Inicio</a></div>
+    </div>
+</nav>
+<div id="general">
+    <?php
+
+    ?>
+</div>
+</body>
+</html>
---
+++ jesuitas.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>inicio</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <nav>
+        <div id="menu">
+            <div id="link-1"><a href="paginaInicio.php">Inicio</a></div>
+            <div id="link-2"><a href="visitas.php">Visitas</a></div>
+            <div id="link-3"><a href="rankingvisitas.php">Rankings</a></div>
+            <div id="link-4"><a href="jesuitas.php">Jesuitas</a></div>
+            <div id="link-5"><a href="logout.php">Cerrar Sesion</a></div>
+        </div>
+    </nav>
+    <div id="general">
+        <?php
+
+        ?>
+    </div>
+</body>
+</html>
